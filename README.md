1a8ddc8b09e34009d69b61e80affd8bb14940b54
Vedang <vedkapse18@gmail.com>
2022-02-22 12:40:58 -0600

    optimised seat allocation

--- README.md
+++ README.md
@@ -11,13 +11,15 @@ safety.
 2. The movie theater has the seating arrangement of 10 rows x 20 seats.
 3. For the purpose of public safety, we assume that a buffer of three
    seats and/or one row is required.
-4. Seats far from the screen are assumed to have a better view.
+4. Seats far from the screen are assumed to provide a better experience.
 5. Screen is nearest to row A and farthest to row J.
+6. Middle seats provide a better experience than the corner seats.
+7. Priority is given to the farther seats and then the middle seats.
 
 ## Solution:
 
 The algorithm assigns seats greedily to the customers on basis of first come first serve strategy.
-The customer who has booked the tickets first will get the better seat.
+The customer who has booked the tickets first will get the better seat i.e far from the screen and in middle of the row to get a better view.
 
 ## Building the code:
 
--- src/theatre.cpp
+++ src/theatre.cpp
@@ -17,7 +17,7 @@ vector<reservation> allocateSeat(vector<reservation> &reservations, vector<row> 
     int allocatedSeats = 0;
     char curRow = 'J';
     int rowItr = 0;
-    int seatItr = 0;
+    int seatItr = 9;
     for (int reservationItr = 0; reservationItr < n; reservationItr++)
     {
         int totalSeats = reservations[reservationItr].totalSeats;
@@ -28,21 +28,30 @@ vector<reservation> allocateSeat(vector<reservation> &reservations, vector<row> 
             continue;
         }
         allocatedSeats += totalSeats;
+
+        //available is even => subtract else add rowItr -+ 4*(5-available)
         while (totalSeats--)
         {
-            rows[rowItr].available--;
+            if (rows[rowItr].available % 2 == 0)
+            {
+                seatItr -= 4 * (5 - rows[rowItr].available);
+            }
+            else
+            {
+                seatItr += 4 * (5 - rows[rowItr].available);
+            }
             rows[rowItr].seats[seatItr] = true;
-
+            rows[rowItr].available--;
             string allocatedSeat = curRow + to_string(seatItr);
             reservations[reservationItr].seatAllocations.push_back(allocatedSeat);
-            seatItr += 4;
+
             if (rows[rowItr].available == 0)
             {
                 rowItr++;
                 if (rowItr % 2 == 0)
-                    seatItr = 0;
+                    seatItr = 9;
                 else
-                    seatItr = 1;
+                    seatItr = 10;
                 curRow--;
             }
         }
